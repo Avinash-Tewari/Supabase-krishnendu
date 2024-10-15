57f6e2138b511e004c7d1c09207955e532ed00f8
Avinash_Tewari <avinashtewari03@gmail.com>
2024-10-16 01:07:33 +0530

    updated

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,40 @@
+import supabase from "../config/supabaseClient"
+import { useEffect,useState } from "react"
+
 const Home = () => {
+  const[fetchError, setFetchError] = useState(null)
+  const[Cars, setCars] = useState(null)
+
+  useEffect(() => {
+    const fetchCars = async () =>{
+      const{ data, error} = await supabase
+      .from(Cars)
+      .select()
+
+      if (error){
+        setFetchError('could not fetch car')
+        setCars(null)
+        console.log(error)
+      }
+      if (data){
+        setCars(data)
+        setFetchError(null)
+      }
+    }
+
+    fetchCars()
+  }, [])
+
   return (
     <div className="page home">
-      <h2>krishnendu has a small dick </h2>
+      {fetchError && (<p>{fetchError}</p>)}\
+      {Cars && (
+        <div className="Cars">
+          {Cars.map(Cars => (
+            <p>{Cars.title}</p>
+          ))}
+          </div>
+      )}
     </div>
   )
 }
